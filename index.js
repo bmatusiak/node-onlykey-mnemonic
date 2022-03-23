6251f5a1fe8eb9c6d3aa49d2350fa07e8c7de32a
Bradley Matusiak <bmatusiak@gmail.com>
2022-03-23 11:01:40 -0400

    updated lib

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var crypto = require("crypto");
 var bip39 = require("bip39");
+var onlykey = require("@trustcrypto/node-onlykey");
 
 module.exports = function(additionalData) {
 
@@ -11,42 +12,10 @@ module.exports = function(additionalData) {
 
         var onlykey_keytype = 1;
 
-        var plugins = [];
-
-        plugins.push(require("@trustcrypto/node-onlykey/src/window.js")); //load replacement onlykey need for plugin
-
-        plugins.push(require("@trustcrypto/node-onlykey/src/onlykey-fido2/plugin.js")); //load onlykey plugin for testing
-
-        plugins.push(require("@trustcrypto/node-onlykey/src/console/console.js")); //load replacement onlykey need for plugin
-
-        var EventEmitter = require("events").EventEmitter;
-
-        var architect = require("@trustcrypto/node-onlykey/libs/architect.js");
-
-
-        plugins.push({
-            provides: ["app"],
-            consumes: ["hub"],
-            setup: function(options, imports, register) {
-                register(null, {
-                    app: new EventEmitter()
-                });
-            }
-        });
-
-        architect.createApp(plugins, function(err, app) {
-
-            if (err) return console.error(err);
-            app.services.app.core = app.services;
-            for (var i in app.services) {
-                app.services.app[i] = app.services[i];
-            }
-            for (var i in app.services) {
-                if (app.services[i].init) app.services[i].init(app);
-            }
-
-            var ONLYKEY = app.services.onlykey3rd(onlykey_keytype);
-
+        onlykey(function(onlykey3rd) {
+            
+            var ONLYKEY = onlykey3rd(onlykey_keytype);
+            
             ONLYKEY.derive_public_key(additionalData, onlykey_keytype, true, async function(err, key) {
                 ONLYKEY.derive_shared_secret(additionalData, key, onlykey_keytype, true, async function(err, sharedSecret) {
                     var hex_secret = crypto.createHash('sha256').update(sharedSecret).digest();
@@ -55,9 +24,7 @@ module.exports = function(additionalData) {
                 });
             });
 
-
         });
-
 
     });
 };
